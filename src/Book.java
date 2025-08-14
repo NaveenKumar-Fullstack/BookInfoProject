9d0312877ce2008d4e25f05f884904dfce3884eb
NaveenKumar-Fullstack <naveenkumargir744@gmail.com>
2025-08-14 12:50:21 +0530

    Initial commit:Set Up BookInfoProject with Book class

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    public static  void main(String[] args){
+        String title;
+        String author;
+        int publication_year;
+
+        title = "The Great Gatsby";
+        author = "F.Scott Fitzgerald";
+        publication_year = 1925;
+
+        System.out.println("Title: "+title+"author: "+author+"publication_year: "+publication_year);
+
+    }
+}
